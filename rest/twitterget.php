69467b0cf246d920939491de5ff31de05263e125
loldongs <yxia@utexas.edu>
2014-05-11 10:40:06 -0500

    Quick Fix
    Fixed some bugs

--- rest/twitterget.php
+++ rest/twitterget.php
@@ -9,10 +9,10 @@ $requestId = $_GET["id"];
 
 require_once("twitteroauth/twitteroauth.php"); 
 
-$consumerkey = "l3eRzucF9L3lR2oEqQVFws5ZI"; 
-$consumersecret = "x9OYTIMMFjyHGOqAodUs0DJv4TnYHqoHEPkDbj8Md9mXLMwX27"; 
-$accesstoken = "53006355-Wokd2v1wn15ywFI8tqXn4fFm3daxZK9ppZYNkNBCk"; 
-$accesstokensecret = "AF3C58AcPeBODPZH3eLI6Yi2TSNq202Zx8z6mPTwPrDNg"; 
+$consumerkey = ""; 
+$consumersecret = ""; 
+$accesstoken = ""; 
+$accesstokensecret = ""; 
 
 function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
   $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
